9c86b5e6b44e21361f33ca1db33659ac3c768d11
BuildTools <marco.schmidt03@gmail.com>
2022-02-08 20:16:40 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
